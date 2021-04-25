5a8341f32987942454acf42efa81b64ad4cc6e9e
Oleg0180 <oksanabakin656@gmail.com>
2021-04-25 10:49:06 +0000

    started projact

---
+++ js/carousel.js
@@ -0,0 +1,32 @@
+const images = [
+    "img/black-guitar.jpg",
+    "img/saxophone.jpg",
+    "img/tambourine.jpg",
+    "img/red-guitar.jpg",
+    "img/drum.jpg"
+]
+
+let currentSlide = 0;
+
+function showSlide() {
+    const carouselImage = document.querySelector('.products-carousel img');
+    carouselImage.src = images[currentSlide];
+}
+
+function nextSlide() {
+    currentSlide++;
+    if (currentSlide >= images.length) currentSlide = 0;
+    showSlide();
+}
+
+function prevSlide() {
+    currentSlide--;
+    if (currentSlide < 0) currentSlide = images.length - 1;
+    showSlide();
+}
+
+showSlide();
+
+
+document.querySelector('.slide-prev').addEventListener('click', prevSlide);
+document.querySelector('.slide-next').addEventListener('click', nextSlide);
